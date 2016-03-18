973e8ead2bbf1bb042fda45b25aa4e8cc39a6bc2
Shivam Bajpai <shivam.bajpai@stackexpress.com>
2016-03-18 03:05:05 -0700

    add idle tracker and cross domain message exchange

---
+++ public/js/start_idle_tracker.js
@@ -0,0 +1,30 @@
+(function ($) {
+    change_idle('Initial');
+
+    $( document ).on( "idle.idleTimer", function(event, elem, obj){
+        change_idle('idle');
+    });
+
+    $( document ).on( "active.idleTimer", function(event, elem, obj, triggerevent){
+        change_idle('active');
+    });
+
+    function change_idle(idle_state){
+        idle_status=idle_state;
+    };
+
+    $.idleTimer(3000);
+
+    respondToIdleStatus = function(e) {
+            updateMsg(e.data)
+        // if (e.origin == '*') {
+            // e.data is the string sent by the origin with postMessage. 
+            if (e.data == 'ruidle?') {
+                parent.postMessage('idle_status:' + idle_status, '*');
+            }
+        // }
+    }
+
+    window.addEventListener('message', respondToIdleStatus, false);
+    
+})(jQuery);
